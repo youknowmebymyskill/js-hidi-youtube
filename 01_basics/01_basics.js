52d6068a8a4cfb4f9bc7990a8b5b7e454e4d407c
youknowmebymyskill <84239385+youknowmebymyskill@users.noreply.github.com>
2025-05-16 08:06:19 +0000

    Your descriptive

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,14 @@
+const accountId = 144556
+let accountEmail = "gaharwarsurya0@gmail.com" 
+var accountpassword ="12345"
+accountcity ="Jaipur"
+//accountId = 2 not allowed
+let accountstate
+accountId=90
+
+accountEmail ="gaharwarsurya0@google.com" 
+console.log(accountId);
+console.table([accountEmail,accountId,accountpassword,accountcity,accountstate])
+/*
+prefer niot to use var  because of issue in block scope and functional scope 
+*/
---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat alll code as newer javascript
+//alert( 3+3) why ?- we are using nodejs not browser 
+console.log(3+3)//code readabilityy  shoulkd eb good 
+console.log("surya")
+let name="surya"
+let age =16
+let isloggedin = true 
+
+//number =>2 to power 52 
+//bigint
+//string
+//boolean=>true/false   
+//null=>standalone value
+let state1 =null;
+//undefined=>value not assigned
+//example of undefined is 
+let state;
+//symbol=>unique value  
+console.log(typeof name)//string
+console.log(typeof null)//number         
